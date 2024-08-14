2d9547c288e9d8377ca3b86845ecf8e39a4c68fc
Vic Pues <141432283+vicpues@users.noreply.github.com>
2024-08-14 15:52:36 +0200

    build: Configure webpack for fonts

--- webpack.config.js
+++ webpack.config.js
@@ -36,6 +36,10 @@ module.exports = {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
       },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
     ],
   }
 };
