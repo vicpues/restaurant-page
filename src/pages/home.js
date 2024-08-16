d1e90554b7f35a3583eb74ad677941549828d048
Vic Pues <141432283+vicpues@users.noreply.github.com>
2024-08-16 16:05:29 +0200

    feat: Add header article

--- src/pages/home.js
+++ src/pages/home.js
@@ -4,6 +4,21 @@ export default function generateHomePage() {
 
     const content = [];
 
+    const article1 = (function() {
+        const article = createArticle();
+
+        const header = document.createElement("h2");
+        header.textContent = "Welcome to Alternian Provisions";
+        article.appendChild(header);
+
+        const para = createParagraph(`"Where every bite immediately follows the last"`)
+        article.appendChild(para);
+
+        return article;
+    })();
+    content.push(article1);
+
+
     const article2 = (function() {
         const article = createArticle();
         const figure = document.createElement("figure");
@@ -17,18 +32,20 @@ export default function generateHomePage() {
         const caption = document.createElement("figcaption");
         caption.textContent = "Please enjoy these delightful and uplifting messages, courtesy of our commercial partners";
         figure.appendChild(caption);
-        
+
         article.appendChild(figure);
 
         return article;
     })();
     content.push(article2);
 
+
     const article3 = createArticle();
     article3.appendChild(createHeader("Our mission"));
     article3.appendChild(createParagraph("Alternian Provisions started with a simple dream: to deliver strictly edible matter to your hunger flap and extract a market-equivalent amount of boondollars from your porkhollow portfolio. Yum."));
     article3.appendChild(createParagraph("From its humble beginnings of not yet existing, Alternian Provisions has expanded into a thriving, FDA compliant establishment serving double-digits of customers every day."));
     content.push(article3);
+
 
     const article4 = createArticle();
     article4.appendChild(createHeader("Opening hours"));
@@ -42,6 +59,7 @@ export default function generateHomePage() {
     article4.appendChild(createParagraph("Sunday: 21:25 - 21:43"));
     content.push(article4);
 
+    
     function createArticle() {
         const article = document.createElement("article");
         article.classList.add("body-article");
