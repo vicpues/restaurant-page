5a2ff1f82007ac737f8474d00954444abbdfa059
Vic Pues <141432283+vicpues@users.noreply.github.com>
2024-08-16 15:35:45 +0200

    feat: Create basic article

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,6 +2,11 @@ export default function generateHomePage() {
 
     const content = [];
 
+    const article3 = createArticle()
+    article3.appendChild(createParagraph("Alternian Provisions started with a simple dream: to deliver strictly edible matter to your hunger flap and extract a market-equivalent amount of boondollars from your porkhollow portfolio. Yum."));
+    article3.appendChild(createParagraph("From its humble beginnings of not yet existing, Alternian Provisions has expanded into a thriving, <em>FDA compliant</em> establishment serving double-digits of customers every day."));
+    content.push(article3);
+
     function createArticle() {
         const article = document.createElement("article");
         article.classList.add("body-article");
