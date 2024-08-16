41b3a9c6f44c9d087463f550d2b3149d9160ba1c
Vic Pues <141432283+vicpues@users.noreply.github.com>
2024-08-16 13:00:27 +0200

    feat: Add clearContent function and cache content

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
 import "./styles/index.css"
 import loadHomePage from "./pages/home.js"
 
+const contentElement = document.querySelector("#content");
 loadHomePage();
+
+
+function clearContent() {
+    while (contentElement.firstChild) {
+        contentElement.removeChild(contentElement.lastChild);
+    };
+}
