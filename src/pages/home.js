d5de8dfc9eb0a8e713b8414d746044e324d7f5b4
Vic Pues <141432283+vicpues@users.noreply.github.com>
2024-08-16 15:33:14 +0200

    feat: Make functions to create paragraphs and articles

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,6 +1,18 @@
-export default loadHomePage = function() {
+export default function generateHomePage() {
 
     const content = [];
+
+    function createArticle() {
+        const article = document.createElement("article");
+        article.classList.add("body-article");
+        return article;
+    }
+
+    function createParagraph(text) {
+        const para = document.createElement("p");
+        para.textContent = text;
+        return para;
+    }
 
     return content;
 }
