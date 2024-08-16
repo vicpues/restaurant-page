04a73796422cdc33c54b144aeda119e11b8c7dbc
Vic Pues <141432283+vicpues@users.noreply.github.com>
2024-08-16 15:39:05 +0200

    fix: Remove difficult <em> tag from paragraph

--- src/pages/home.js
+++ src/pages/home.js
@@ -5,7 +5,7 @@ export default function generateHomePage() {
     const article3 = createArticle()
     article3.appendChild(createHeader("Our mission"));
     article3.appendChild(createParagraph("Alternian Provisions started with a simple dream: to deliver strictly edible matter to your hunger flap and extract a market-equivalent amount of boondollars from your porkhollow portfolio. Yum."));
-    article3.appendChild(createParagraph("From its humble beginnings of not yet existing, Alternian Provisions has expanded into a thriving, <em>FDA compliant</em> establishment serving double-digits of customers every day."));
+    article3.appendChild(createParagraph("From its humble beginnings of not yet existing, Alternian Provisions has expanded into a thriving, FDA compliant establishment serving double-digits of customers every day."));
     content.push(article3);
 
     function createArticle() {
