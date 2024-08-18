a7d2fc32185da22874cbaece60ed0e3c81090e1e
Vic Pues <141432283+vicpues@users.noreply.github.com>
2024-08-18 17:30:51 +0200

    refactor: Refactor element variables into dom object

--- src/index.js
+++ src/index.js
@@ -3,11 +3,25 @@ import homePage from "./pages/home.js";
 import aboutPage from "./pages/about.js";
 import menuPage from "./pages/menu.js";
 
-// Cache content section
-const contentElement = document.querySelector("#content");
-
-
+// Main section
+const dom = cacheDom();
 replaceContent(homePage());
+
+
+// Cache dom
+
+function cacheDom() {
+    return {
+        content: document.querySelector("#content"),
+        homeButton: document.querySelector("#home-button"),
+        menuButton: document.querySelector("#menu-button"),
+        aboutButton: document.querySelector("#about-button"),
+    }
+}
+
+
+// Events
+
 
 
 // DOM functions
@@ -18,13 +32,13 @@ function replaceContent(newChildren) {
 }
 
 function clearContent() {
-    while (contentElement.firstChild) {
-        contentElement.removeChild(contentElement.lastChild);
+    while (dom.content.firstChild) {
+        dom.content.removeChild(dom.content.lastChild);
     };
 }
 
 function populateContent(newChildren) {
     for (let child of newChildren) {
-        contentElement.appendChild(child);
+        dom.content.appendChild(child);
     };
 }
