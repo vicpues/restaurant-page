74630f19e9b93e0e8c069aa4a034379cb171e4f0
Vic Pues <141432283+vicpues@users.noreply.github.com>
2024-08-19 16:56:25 +0200

    feat: Generate empty articles in menu page

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -1,7 +1,57 @@
+import { createArticle, createParagraph } from "./page-utils";
+
 export default function generateMenuPage() {
     const content = [];
 
-    
+    content.push((() => {
+        const article = createArticle();
+
+        
+
+        return article;
+    })())
+
+    content.push((() => {
+        const article = createArticle();
+
+        
+
+        return article;
+    })())
+
+    content.push((() => {
+        const article = createArticle();
+
+        
+
+        return article;
+    })())
+
+    content.push((() => {
+        const article = createArticle();
+
+        
+
+        return article;
+    })())
+
+    content.push((() => {
+        const article = createArticle();
+
+        
+
+        return article;
+    })())
+
+    content.push((() => {
+        const article = createArticle();
+
+        
+
+        return article;
+    })())
+
+
 
     return content;
 }
