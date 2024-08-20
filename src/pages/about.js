c5b93bb288d94d442c971a55738da1ed389a3d31
Vic Pues <141432283+vicpues@users.noreply.github.com>
2024-08-20 14:36:38 +0200

    feat: Move mission article to about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,3 +1,17 @@
-export default function generateAboutPage() {
+import { createArticle, createHeader, createParagraph } from "./page-utils";
 
+export default function generateAboutPage() {
+    const content = [];
+
+
+    content.push((() => {
+        const article = createArticle();
+        article.appendChild(createHeader("Our mission"));
+        article.appendChild(createParagraph("Alternian Provisions started with a simple dream: to deliver strictly edible matter to your hunger flap and extract a market-equivalent amount of boondollars from your porkhollow portfolio. Yum."));
+        article.appendChild(createParagraph("From its humble beginnings of not yet existing, Alternian Provisions has expanded into a thriving, FDA compliant establishment serving double-digits of customers every day."));
+        return article;
+    })());
+
+
+    return content;
 }
--- src/pages/home.js
+++ src/pages/home.js
@@ -31,16 +31,6 @@ export default function generateHomePage() {
         return article;
     })());
 
-
-    content.push((() => {
-        const article = createArticle();
-        article.appendChild(createHeader("Our mission"));
-        article.appendChild(createParagraph("Alternian Provisions started with a simple dream: to deliver strictly edible matter to your hunger flap and extract a market-equivalent amount of boondollars from your porkhollow portfolio. Yum."));
-        article.appendChild(createParagraph("From its humble beginnings of not yet existing, Alternian Provisions has expanded into a thriving, FDA compliant establishment serving double-digits of customers every day."));
-        return article;
-    })());
-
-
     content.push((() => {
         const article = createArticle();
         article.appendChild(createHeader("Opening hours"));
